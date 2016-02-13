30e1a7f95412b3d9496acc80a903e2b9acce02b7
tiger4th <tiger4thg@gmail.com>
2016-02-13 20:34:10 +0900

    シークバーの丸を動かす

---
+++ Assets/Scripts/SeekController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeekController : MonoBehaviour {
+	public GameObject Circle;
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveAdd( Circle, new Vector3(588, 0, 0), 180f );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
