1afb08c15aae87212631c2574bb6ca0adfc4eb16
tiger4th <tiger4thg@gmail.com>
2016-02-14 01:53:43 +0900

    タップするまでユニティちゃんが動かないようにする

--- Assets/Scripts/Logo.cs
+++ Assets/Scripts/Logo.cs
@@ -6,10 +6,17 @@ public class Logo : MonoBehaviour, IPointerClickHandler {
 	GameObject MainObject;
 	MainController MainController;
 
+	public GameObject UnityChan;
+	Animator UnityChanAnim;
+
 	void Start() {
 	}
 
 	public void OnPointerClick (PointerEventData eventData){
+		UnityChan = GameObject.Find ("unitychan");
+		UnityChanAnim = UnityChan.GetComponent<Animator>();
+		UnityChanAnim.SetBool ("Next", true);
+
 		MainController = GameObject.Find("Camera").GetComponent<MainController>();;
 		MainController.Movie();
 		Debug.Log ( "OnPointerClick :" + eventData );
