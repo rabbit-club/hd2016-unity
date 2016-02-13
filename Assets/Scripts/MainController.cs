91c8f6a9352ada288f6e0f741b2f0000dfe369ab
tiger4th <tiger4thg@gmail.com>
2016-02-13 14:18:39 +0900

    画像取得

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+	public GameObject Display;
+	SpriteRenderer DisplaySprite;
+	string imageUrl;
+
+	IEnumerator Start ()
+	{
+		DisplaySprite = Display.GetComponent<SpriteRenderer>();
+
+		imageUrl = "amd.c.yimg.jp/amd/20160213-00010005-tennisd-000-1-view.jpg";
+
+		WWW www = new WWW(imageUrl);
+		yield return www;
+
+		DisplaySprite.sprite = Sprite.Create (
+			www.texture, 
+			new Rect (0, 0, 400, 300), 
+			new Vector2(0.5f, 0.5f)
+		);
+	}
+}
