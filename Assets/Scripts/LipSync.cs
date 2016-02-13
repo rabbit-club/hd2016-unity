ba3e4461af4d5d46215510a2a7e87cc5491adff4
tanahara <sat@tanahara-no-MacBook-Air.local>
2016-02-13 21:44:46 +0900

    口パクスクリプト追加

--- Assets/Scripts/LipSync.cs
+++ Assets/Scripts/LipSync.cs
@@ -4,13 +4,30 @@ using System.Linq;
 
 public class LipSync : MonoBehaviour {
 
-	// Use this for initialization
+	SkinnedMeshRenderer skinMesh;
+	float timer;
+	bool mouseClose;
+
 	void Start () {
-	
+		timer = 0;
+		mouseClose = true;
+		skinMesh = this.GetComponentsInChildren<SkinnedMeshRenderer>().First(s => s.name == "MTH_DEF");
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		timer += Time.deltaTime;
+		if(timer > 0.1f) {
+			paku();
+			timer = 0;
+			mouseClose = !mouseClose;
+		}
+	}
+
+	void paku() {
+		if (mouseClose) {
+			skinMesh.SetBlendShapeWeight (0, 0.0f);
+		} else {
+			skinMesh.SetBlendShapeWeight(0, 50.0f);
+		}
 	}
 }
