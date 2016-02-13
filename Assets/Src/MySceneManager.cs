302bdf86335652f65e94694f70f30011e9fe8803
tanahara <sat@tanahara-no-MacBook-Air.local>
2016-02-13 13:09:07 +0900

    見えていない範囲のオブジェクトを非表示に＆マルチシーンに対応。

---
+++ Assets/Src/MySceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MySceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("spaceship", LoadSceneMode.Additive);
+		SceneManager.LoadScene("UIScene", LoadSceneMode.Additive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
